ce35b7969a271f15201c5875a8287d1333800a93
KdsGamer <76777611+KdsGamer@users.noreply.github.com>
2021-04-08 15:11:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var obj2, obj1;
+
+function setup() {
+  createCanvas(1200,800);
+  obj21 = createSprite(400, 100, 50, 80);
+  obj21.shapeColor = "green";
+  obj21.debug = true;
+  obj11 = createSprite(400, 800,80,30);
+  obj11.shapeColor = "green";
+  obj11.debug = true;
+
+  obj11.velocityY = -5;
+  obj21.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+bounceoff
+ 
+  drawSprites();
+}
+function bounceoff(obj1,obj2){
+  if (obj1.x - obj2.x < obj2.width/2 + obj1.width/2
+    && obj2.x - obj1.x < obj2.width/2 + obj1.width/2) {
+  obj1.velocityX = obj1.velocityX * (-1);
+  obj2.velocityX = obj2.velocityX * (-1);
+}
+if (obj1.y - obj2.y < obj2.height/2 + obj1.height/2
+  && obj2.y - obj1.y < obj2.height/2 + obj1.height/2){
+  obj1.velocityY = obj1.velocityY * (-1);
+  obj2.velocityY = obj2.velocityY * (-1);
+}
+}
